c8ea9081ca0820072cf883e4a404bf4081b3f101
Anselmo Lacerda S. de Melo <anselmolsm@gmail.com>
2009-06-22 00:39:42 -0300

    emacs: Initial commit of emacs config file
    Signed-off-by: Anselmo Lacerda S. de Melo <anselmolsm@gmail.com>

---
+++ emacs/init.el
@@ -0,0 +1,121 @@
+
+;; my .emacs
+;;http://emacs-fu.blogspot.com/
+;;http://www.emacswiki.org/
+;;http://www.cs.utah.edu/dept/old/texinfo/emacs18/emacs_20.html
+;;http://homepages.inf.ed.ac.uk/s0243221/emacs/
+;;http://defindit.com/readme_files/emacs_tool_menu_bars.html
+;;https://kb.mit.edu/confluence/display/ist/Disabling+the+Emacs+menubar,+toolbar,+or+scrollbar
+;;http://as760.http.sasm3.net/textbooks/emacs/emacs_20.html
+;;http://www.emacswiki.org/cgi-bin/wiki/sourcepair.el
+;;http://geosoft.no/development/emacs.html
+
+; General stuff ;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; don't show startup messages
+(setq inhibit-startup-message t)
+(setq inhibit-startup-echo-area-message t)
+;; change where backups are stored
+(setq make-backup-files t)
+;(setq version-control t)
+;(setq backup-directory-alist (quote ((".* .~/.emacs_backups/"))))
+
+
+;; column limit
+(setq text-mode-hook 'turn-on-auto-fill); automatically auto-fill
+(setq-default default-fill-column 100)	; 72
+(setq default-comment-column 40)	;
+(setq ask-about-buffer-names t)		; be helpful
+(setq completion-auto-help t)		;    with buffer names
+(setq insert-default-directory t)       ; be helpful with file paths
+(setq default-truncate-lines t)         ; truncate, not wrap, long lines
+(setq default-case-fold-search t)       ; t = upper/lower case same
+(setq next-screen-context-lines 4)      ; overlap when scroll off screen
+
+;; Always end a file with a newline
+(setq require-final-newline t)
+
+
+; Look & feel ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(mouse-wheel-mode t) ; enable mouse wheel
+;; colors
+(set-background-color "black")
+(set-foreground-color "white")
+(set-mouse-color "goldenrod")
+(set-cursor-color "red")
+(global-hl-line-mode 1)
+(set-face-background 'hl-line "#666")
+(transient-mark-mode t)
+(setq search-highlight           t) ; Highlight search object
+(setq query-replace-highlight    t) ; Highlight query object
+(setq mouse-sel-retain-highlight t) ; Keep mouse high-lightening
+(menu-bar-mode 0)      ; gui parameters: no menu
+(tool-bar-mode 0)      ; and no toolbar
+(toggle-scroll-bar 0)  ; no scroll-bar
+(column-number-mode t) ; show column number
+(line-number-mode t)   ; show line number
+
+(setq scroll-step 1)    ; scroll line by line
+(setq visible-bell nil) ; disable visible bell
+(fset 'yes-or-no-p 'y-or-n-p) ; because I can write only y/n instead of yes/no...
+
+;; highlight trailing whitespace
+(setq-default show-trailing-whitespace t)
+(setq-default default-indicate-empty-lines t)
+(setq-default indent-tabs-mode nil)
+
+;; show matching parentheses
+(setq-default show-paren-mode)
+
+;; keyboard shortcuts
+(global-set-key [\C-tab] 'bs-cycle-next) ; Ctrl-Tab and Ctrl-Shift-Tab cycle through buffers
+;(global-set-key [\C-\S-tab] 'bs-cycle-previous)
+(global-set-key "\M-c" 'compile)
+;(global-set-key "\C-x\C-e" 'compile)
+(global-set-key "\C-g" 'goto-line)
+(global-set-key [f5] 'delete-trailing-whitespace)
+;(global-set-key "\C-z" 'undo)
+;(global-set-key "\C-c" 'copy-region-as-kill);;copy
+;(global-set-key "\C-v" 'yank);;paste
+(global-set-key [delete] 'delete-char)
+;(global-set-key [home] 'beginning-of-buffer)
+;(global-set-key [end] 'end-of-buffer)
+(global-set-key [f6] 'ff-find-other-file)
+(global-set-key [f9] 'kill-buffer)
+
+;; C/C++
+(require 'cc-mode)
+(setq-default indent-tabs-mode nil)
+(define-key c-mode-base-map (kbd "RET") 'newline-and-indent)
+(setq-default c-basic-offset 4)
+
+(defun fast-uncomment-region ()
+  (local-set-key [(control c)(control u)] 'uncomment-region))
+
+
+;; C++ autocompletion
+(defconst c++-keywords
+  (sort
+   (list "and" "bool" "compl" "do" "export" "goto" "namespace" "or_eq" "return"
+	 "struct" "try" "using" "xor" "and_eq" "break" "const" "double" "extern"
+	 "if" "new" "private" "short" "switch" "typedef" "virtual" "xor_eq" "asm"
+	 "case" "const_cast" "dyndoamic_cast" "false" "inline" "not" "protected"
+	 "signed" "template" "typeid" "void" "auto" "catch" "continue" "else"
+	 "float" "int" "not_eq" "public" "sizeof" "this" "typename" "volatile"
+	 "bitand" "char" "default" "enum" "for" "long" "operator" "register"
+	 "static" "throw" "union" "wchar_t" "bitor" "class" "delete" "explicit"
+	 "friend" "mutable" "or" "reinterpret_cast" "static_cast" "true"
+	 "unsigned" "while" ) #'(lambda (a b) (> (length a) (length b)))))
+
+(defvar ac-source-c++
+  '((candidates
+     . (lambda ()
+	 (all-completions ac-target c++-keywords))))
+  "Source for c++ keywords.")
+
+(add-hook 'c++-mode-hook
+	  (lambda ()
+	  (make-local-variable 'ac-sources)
+	  (setq ac-sources '(ac-source-c++))))
+
+(iswitchb-mode)
+

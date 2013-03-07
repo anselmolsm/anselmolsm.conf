9214e238b7c76da5dcdb3a731c3e1f722d6cca40
Anselmo L. S. Melo <anselmolsm@gmail.com>
2013-03-07 11:34:03 -0300

    emacs: load path

--- emacs/init.el
+++ emacs/init.el
@@ -15,6 +15,7 @@
 ;;http://www.enigmacurry.com/2009/01/21/autocompleteel-python-code-completion-in-emacs/
 ;;http://www.emacswiki.org/emacs/CommentingCode
 
+(add-to-list 'load-path "~/.emacs.d/")
 (add-to-list 'load-path "~/.emacs.d/lisp/")
 
 ; General stuff ;;;;;;;;;;;;;;;;;;;;;;;;;;

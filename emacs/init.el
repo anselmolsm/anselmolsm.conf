264a6dcbd4fc17757eccb959682696c85159421b
Anselmo L. S. Melo <anselmolsm@gmail.com>
2013-03-11 08:53:43 -0300

    Emacs: csharp-mode++

--- emacs/init.el
+++ emacs/init.el
@@ -65,6 +65,7 @@
 (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 
+(require 'csharp-mode)
 (autoload 'csharp-mode "csharp-mode" "C# Mode." t)
 (add-to-list 'auto-mode-alist '("\\.cs\\'" . csharp-mode))
 

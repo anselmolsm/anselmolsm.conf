90433dd643d20f8e6027d1c7727bf823b83707f6
Anselmo L. S. Melo <anselmolsm@gmail.com>
2013-12-10 22:13:15 -0200

    Fish: more config

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,3 @@
-#alias grep='grep --colour=auto --exclude=*.svn-base'
-alias rgrep='grep -R'
-
-
 # Fish git prompt
 set __fish_git_prompt_showdirtystate 'yes'
 set __fish_git_prompt_showstashstate 'yes'
@@ -26,3 +22,39 @@ end
 
 #no welcome message
 set fish_greeting ""
+
+set -U EDITOR nw
+
+
+set -U NODE_fish_user_paths /usr/lib/node_modules
+
+##aliases####
+alias rgrep='grep -R'
+# for security reasons
+alias rm='rm -i'
+alias mv='mv -i'
+
+# simplifying common commands
+alias gvim='gvim -p'
+alias v='vim'
+alias ll='ls -lh'
+alias nw='emacs -nw'
+
+# boring typo
+alias sl='ls'
+
+# using colors!
+alias vless='vim -u /usr/share/vim/vim71/macros/less.vim'
+
+# I always forget 'time' when I wanna measure build time..
+alias make='time make'
+
+# archlinux: always use yaourt
+alias pacman='yaourt'
+
+# git achievements
+alias git="git-achievements"
+
+set -Ux fish_user_paths /usr/lib/icecream/bin $HOME/local/bin /usr/local/bin $PATH
+set -Ux fish_user_paths $PATH $HOME/git-achievements/
+set -Ux fish_user_paths $PATH /home/anselmo/.gem/ruby/2.0.0/bin

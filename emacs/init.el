752acaee9308bc4d1253bff4ee0e1b8e520baa5c
Anselmo L. S. Melo <anselmolsm@gmail.com>
2015-03-01 12:14:09 -0300

    Emacs: bb-mode

--- emacs/init.el
+++ emacs/init.el
@@ -399,4 +399,12 @@
 (global-semantic-highlight-func-mode 1)
 (global-semantic-idle-breadcrumbs-mode 1)
 
+;; bitbake mode - https://raw.githubusercontent.com/mferland/bb-mode/master/bb-mode.el
+(require 'bb-mode)
+(setq auto-mode-alist (cons '("\\.bb$" . bb-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.inc$" . bb-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.bbappend$" . bb-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.bbclass$" . bb-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.conf$" . bb-mode) auto-mode-alist))
+
 ;;; init.el ends here
